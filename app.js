2381e852ef505ece38e9e65418623cdc5dc0fc6d
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 04:57:23 +0500

    Question number 14
    This file contains a solution to question number 14 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var transportation = ["Bugatti Chiron", "Hennessey Venom F5", "Range Rover"];
-for (var i = 0; i < transportation.length; i++) {
-    console.log("I would like to own a " + transportation[i] + ".");
+var guestList = ["Tom Cruise", "Angelina Jolie", "Denzel Washington"];
+for (var i = 0; i < guestList.length; i++) {
+    console.log("Dear " + guestList[i] + ", you are invited to dinner. We would be honored to have you join us.");
 }
+//Question # 19: Dinner Guests
+console.log("Number of poeple invited to dinner:", guestList.length);
