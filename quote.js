67826eace75b917a6289d8119da4418f95d5f469
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 04:51:03 +0500

    Question number 4
    This file contains a solution to question number 4 of the 45 questions assignment for the Governor Sindh Initiative IT course.

---
+++ quote.js
@@ -0,0 +1,7 @@
+//Program written by Mariyam Asif
+//Date: 04-03-2024
+//This program prints out a quote by Nelson Mandela.
+
+var quote = "It always seems impossible until it's done.";
+var author = "Nelson Mandela";
+console.log("".concat(author, " once said \"").concat(quote, "\""));
---
+++ quote.ts
@@ -0,0 +1,3 @@
+let quote= "It always seems impossible until it's done.";
+let author= "Nelson Mandela";
+console.log(`${author} once said "${quote}"`);
