44ae60452d8e9431967fafd2a4f26d68f0a7afcf
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 04:54:38 +0500

    Question number 11
    This file contains a solution to question number 11 of the 45 questions assignment for the Governor Sindh Initiative IT course.

---
+++ name.js
@@ -0,0 +1,4 @@
+var names = ["Saira", "Bashayer", "Sabahat"];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ name.ts
@@ -0,0 +1,4 @@
+let names: string[] = ["Saira", "Bashayer", "Sabahat"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
