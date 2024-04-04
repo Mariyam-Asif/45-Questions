650ace72b105e85833d533104f88b7fb8bcdc35f
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:06:00 +0500

    Question number 33
    This file contains a solution to question number 33 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 "use strict";
-let current_users = ['john', 'sarah', 'michael', 'emma', 'kate'];
-let new_users = ['peter', 'sarah', 'adam', 'ryan', 'john'];
-for (let i = 0; i < new_users.length; i++) {
-    let lowercase_current_users = current_users.map(user => user.toLowerCase());
-    let lowercase_new_users = new_users[i].toLowerCase();
-    if (lowercase_current_users.includes(lowercase_new_users)) {
-        console.log("Sorry, the username '" + new_users[i] + "' is already taken. Please enter a new username.");
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = 0; i < numbers.length; i++) {
+    let number = numbers[i];
+    if (number === 1) {
+        console.log(number + "st");
+    }
+    else if (number === 2) {
+        console.log(number + "nd");
+    }
+    else if (number === 3) {
+        console.log(number + "rd");
     }
     else {
-        console.log("The username '" + new_users[i] + "' is available.");
+        console.log(number + "th");
     }
 }
--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
-let current_users = ['john', 'sarah', 'michael', 'emma', 'kate'];
-let new_users = ['peter', 'sarah', 'adam', 'ryan', 'john'];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for (let i = 0; i < new_users.length; i++) {
-    let lowercase_current_users = current_users.map(user => user.toLowerCase());
-    let lowercase_new_users = new_users[i].toLowerCase();
+for (let i =0; i < numbers.length; i++) {
+    
+    let number = numbers[i];
 
-    if (lowercase_current_users.includes(lowercase_new_users)) {
-        console.log("Sorry, the username '" + new_users[i] + "' is already taken. Please enter a new username.");
+    if (number === 1) {
+        console.log(number + "st");
+    } else if (number === 2) {
+        console.log(number + "nd");
+    } else if (number === 3) {
+        console.log(number + "rd");
     } else {
-        console.log("The username '" + new_users[i] + "' is available.");
+        console.log(number + "th");
     }
 }
