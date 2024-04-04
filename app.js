16f19a62121fc235d174c2f8883c3abee6c01727
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:08:41 +0500

    Question number 38
    This file contains a solution to question number 38 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
-function make_shirts(size = 'large', message = 'I love TypeScript') {
-    console.log("The shirt size is " + size + " and the message printed on it is: " + message);
+function describe_city(city, country = "Pakistan") {
+    console.log(city + " is in " + country + ".");
 }
-make_shirts();
-make_shirts('medium');
-make_shirts('small', 'Fearless');
+describe_city("Karachi");
+describe_city("Islamabad");
+describe_city("Seoul", "Korea");
