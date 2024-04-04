23b112a42c061ed128fcc04b37e34ea496d5a119
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:05:06 +0500

    Question number 31
    This file contains a solution to question number 31 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 "use strict";
-let usernames = ['john', 'michael', 'admin', 'sarah', 'emma'];
-for (let i = 0; i < usernames.length; i++) {
-    if (usernames[i] === 'admin') {
-        console.log("Hello admin, would you like to see a status report?");
+let usernames = ['john', "sarah", "admin", "michael", "emma"];
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    for (let i = 0; i < usernames.length; i++) {
+        if (usernames[i] === "admin") {
+            console.log("Hello admin, would you like to see a status report?");
+        }
+        else {
+            console.log("Hello " + usernames[i] + ", thank you for logging in again.");
+        }
     }
-    else {
-        console.log("Hello " + usernames[i] + ", thank you for logging in again.");
-    }
+}
+usernames = [];
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
 }
