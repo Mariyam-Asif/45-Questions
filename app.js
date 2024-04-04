2c0d575e07e67ad9aecf21fc6293c298f40edea3
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:01:04 +0500

    Question number 23
    This file contains a solution to question number 23 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-var numbers = [1, 2, 3, 4, 5];
-//Index Error
-console.log(numbers[10]);
-console.log(numbers[3]);
+var car = 'bugatti';
+//Test 1
+console.log("Is car == 'bugatti'? I predict True.");
+console.log(car == 'bugatti');
+//Test 2
+console.log("Is car != 'honda'? I predict True.");
+console.log(car != 'honda');
+//Test 3
+console.log("Is car === 'bugatti'? I predict True.");
+console.log(car === 'bugatti');
+//Test 4
+console.log("Is car < 'toyota'? I predict True.");
+console.log(car < 'toyota');
+//Test 5
+console.log("Is car <= 'bugatti'? I predict True.");
+console.log(car <= 'bugatti');
+//Test 6
+console.log("Is car == 'honda'? I predict False.");
+console.log(car == 'honda');
+//Test 7
+console.log("Is car == 'Bugatti'? I predict False.");
+console.log(car == 'Bugatti');
+//Test 8
+console.log("Is car == undefined? I predict False.");
+console.log(car == undefined);
+//Test 9
+console.log("Is car !== 'bugatti'? I predict False.");
+console.log(car !== 'bugatti');
+//Test 10
+console.log("Is car >= 'ford'? I predict False.");
+console.log(car >= 'ford');
--- app.ts
+++ app.ts
@@ -1,6 +1,42 @@
-let numbers: number[] = [1, 2, 3, 4, 5];
+let car = 'bugatti';
 
-//Index Error
-console.log(numbers[10]);
+//Test 1
+console.log("Is car == 'bugatti'? I predict True.");
+console.log(car == 'bugatti');
 
-console.log(numbers[3]);
+//Test 2
+console.log("Is car != 'honda'? I predict True.");
+console.log(car != 'honda');
+
+//Test 3
+console.log("Is car === 'bugatti'? I predict True.");
+console.log(car === 'bugatti');
+
+//Test 4
+console.log("Is car < 'toyota'? I predict True.");
+console.log(car < 'toyota');
+
+//Test 5
+console.log("Is car <= 'bugatti'? I predict True.");
+console.log(car <= 'bugatti');
+
+//Test 6
+console.log("Is car == 'honda'? I predict False.");
+console.log(car == 'honda');
+
+//Test 7
+console.log("Is car == 'Bugatti'? I predict False.");
+console.log(car == 'Bugatti');
+
+//Test 8
+console.log("Is car == undefined? I predict False.")
+console.log(car == undefined);
+
+//Test 9
+console.log("Is car !== 'bugatti'? I predict False.");
+console.log(car !== 'bugatti');
+
+//Test 10
+console.log("Is car >= 'ford'? I predict False.");
+console.log(car >= 'ford');
+
