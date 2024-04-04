0ea436ffc7e4f100c1213feab91ea37e5a1ae586
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:08:10 +0500

    Question number 37
    This file contains a solution to question number 37 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
-function make_shirt(size, message) {
-    console.log("The size of the shirt is " + size + " and the message printed on it is: " + message);
+function make_shirts(size = 'large', message = 'I love TypeScript') {
+    console.log("The shirt size is " + size + " and the message printed on it is: " + message);
 }
-make_shirt("medium", "Dare to Dream");
+make_shirts();
+make_shirts('medium');
+make_shirts('small', 'Fearless');
--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
-function make_shirt(size: string, message: string) {
-    console.log("The size of the shirt is " + size + " and the message printed on it is: " + message);
+function make_shirts(size = 'large', message = 'I love TypeScript') {
+    console.log("The shirt size is " + size + " and the message printed on it is: " + message);
 }
-make_shirt("medium", "Dare to Dream");
+
+make_shirts();
+make_shirts('medium');
+make_shirts('small', 'Fearless');
