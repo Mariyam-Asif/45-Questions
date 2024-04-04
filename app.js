4e8df6cf8cac7fa9ad497964a33b2e09316983fa
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:10:45 +0500

    Question number 41
    This file contains a solution to question number 41 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
 "use strict";
-function make_album(artist, title) {
-    return {
-        artist,
-        title
-    };
+let magicians = ['David Copperfield', 'Penn Jillette', 'Teller'];
+function show_magicians(magicians) {
+    for (let i = 0; i < magicians.length; i++) {
+        console.log(magicians[i]);
+    }
 }
-let album1 = make_album("Talha Anjum", "Talha Anjum Mixtape");
-let album2 = make_album("August D", "August D Mixtape");
-let album3 = make_album("BTS", "Proof");
-console.log(album1);
-console.log(album2);
-console.log(album3);
-function make_album2(artist, title, tracks) {
-    return {
-        artist,
-        title,
-        tracks
-    };
-}
-let album4 = make_album2("Atif Aslam", "Jal Pari", 8);
-let album5 = make_album2("Vital Signs", "Vital Signs 1", 9);
-let album6 = make_album2("Shehzad Roy", "Qismat Apney Haath Mein", 12);
-console.log(album4);
-console.log(album5);
-console.log(album6);
+show_magicians(magicians);
--- app.ts
+++ app.ts
@@ -1,30 +1,9 @@
-function make_album(artist: string, title: string) {
-    return {
-        artist,
-        title
-    };
+let magicians: string[] = ['David Copperfield', 'Penn Jillette', 'Teller'];
+
+function show_magicians(magicians: string[]) {
+    for (let i = 0; i < magicians.length; i++) {
+        console.log(magicians[i]);
+    }
 }
 
-let album1 = make_album("Talha Anjum", "Talha Anjum Mixtape");
-let album2 = make_album("August D", "August D Mixtape");
-let album3 = make_album("BTS", "Proof");
-
-console.log(album1);
-console.log(album2);
-console.log(album3);
-
-function make_album2(artist:string, title:string, tracks: number) {
-    return {
-        artist,
-        title,
-        tracks
-    };
-}
-
-let album4 = make_album2("Atif Aslam", "Jal Pari", 8);
-let album5 = make_album2("Vital Signs", "Vital Signs 1", 9);
-let album6 = make_album2("Shehzad Roy", "Qismat Apney Haath Mein", 12);
-
-console.log(album4);
-console.log(album5);
-console.log(album6);
+show_magicians(magicians);
