51aa2dc58a87e915360b70979f094c56b9ec7135
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:02:41 +0500

    Question number 26
    This file contains a solution to question number 26 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.ts
+++ app.ts
@@ -1,13 +1,17 @@
-//Version #1
+//Version # 1
 let alien_color1 = 'green';
 
 if (alien_color1 === 'green') {
-    console.log("Congratulations! The player just earned 5 points.");
+    console.log("Congratulations! The player just earned 5 points for shooting the alien.");
+} else {
+    console.log("Congratulations! The player just earned 10 points.");
 }
 
-//Version #2
+//Version # 2
 let alien_color2 = 'red';
 
-if (alien_color2 === 'yellow') {
-    console.log("Congratulations! The player just earned 5 points.");
+if (alien_color2 === 'green') { 
+    console.log("Congratulations! The player just earned 5 points for shooting the alien.");
+} else {
+    console.log("Congratulations! The player just earned 10 points.");
 }
