3f287c8edae4541d0a7eea05779fa57040c76930
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:03:12 +0500

    Question number 27
    This file contains a solution to question number 27 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.ts
+++ app.ts
@@ -1,17 +1,31 @@
-//Version # 1
-let alien_color1 = 'green';
+let alien_colors1 = 'green';
 
-if (alien_color1 === 'green') {
-    console.log("Congratulations! The player just earned 5 points for shooting the alien.");
-} else {
+if (alien_colors1 === 'green') {
+    console.log("Congratulations! The player just earned 5 points.");
+} else if (alien_colors1 === 'yellow') {
     console.log("Congratulations! The player just earned 10 points.");
+} else if (alien_colors1 === 'red') {
+    console.log("Congratulations! The player just earned 15 points.");
 }
 
 //Version # 2
-let alien_color2 = 'red';
+let alien_colors2 = 'yellow';
 
-if (alien_color2 === 'green') { 
-    console.log("Congratulations! The player just earned 5 points for shooting the alien.");
-} else {
+if (alien_colors2 === 'green') {
+    console.log("Congratulations! The player just earned 5 points.");
+} else if (alien_colors2 === 'yellow') {
     console.log("Congratulations! The player just earned 10 points.");
+} else if (alien_colors2 === 'red') {
+    console.log("Congratulations! The player just earned 15 points.");
+}
+
+//Version # 3
+let alien_colors3 = 'red'
+
+if (alien_colors3 === 'green') {
+    console.log("Congratulations! The player just earned 5 points.");
+} else if (alien_colors3 === 'yellow') {
+    console.log("Congratulations! The player just earned 10 points.");
+} else if (alien_colors3 === 'red') {
+    console.log("Congratulations! The player just earned 15 points.");
 }
