4d62afcac8b25f5465a5d27162fd43a016371416
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:06:31 +0500

    Question number 34
    This file contains a solution to question number 34 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 "use strict";
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-for (let i = 0; i < numbers.length; i++) {
-    let number = numbers[i];
-    if (number === 1) {
-        console.log(number + "st");
-    }
-    else if (number === 2) {
-        console.log(number + "nd");
-    }
-    else if (number === 3) {
-        console.log(number + "rd");
-    }
-    else {
-        console.log(number + "th");
-    }
+let pizzas = ["mexican", "kebab", "bbq"];
+for (let i = 0; i < pizzas.length; i++) {
+    console.log("I like " + pizzas[i] + " pizza.");
 }
+console.log("I really love pizza!");
