e6c0fd3ceb82ef3e19cf7fedc33e368a1dfa1111
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:00:13 +0500

    Question number 21
    This file contains a solution to question number 21 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-var myList = ["Seoul", "Dubai", "Sydney", "New York", "Dallas", "Los Angeles"];
-console.log("My list of cities:", myList);
+var Item1 = {
+    name: "Mount Everest",
+    category: "Mountain",
+    description: "The highest mountain in the world."
+};
+var Item2 = {
+    name: "Nile River",
+    category: "River",
+    description: "The largest river in the world."
+};
+var Item3 = {
+    name: "Great Wall of China",
+    category: "Landmark",
+    description: " An ancient wall in China."
+};
+console.log("Item 1:", Item1);
+console.log("Item 2:", Item2);
+console.log("Item 3:", Item3);
--- app.ts
+++ app.ts
@@ -1,2 +1,30 @@
-let myList: string[] = ["Seoul", "Dubai", "Sydney", "New York", "Dallas", "Los Angeles"];
-console.log("My list of cities:", myList);
+//Structure
+type Item = { 
+    name: string;
+    category: string;
+    description: string;
+};
+
+let Item1: Item = {
+    name: "Mount Everest",
+    category: "Mountain",
+    description: "The highest mountain in the world."
+};
+
+let Item2: Item = {
+    name: "Nile River",
+    category: "River",
+    description: "The largest river in the world."
+};
+
+let Item3: Item = {
+    name: "Great Wall of China",
+    category: "Landmark",
+    description: " An ancient wall in China."
+};
+
+console.log("Item 1:", Item1);
+console.log("Item 2:", Item2);
+console.log("Item 3:", Item3);
+
+
