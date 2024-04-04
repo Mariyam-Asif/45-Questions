2842dec0cf12c33a771ca000d79cd079566acdd3
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:11:09 +0500

    Question number 42
    This file contains a solution to question number 42 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 "use strict";
-let magicians = ['David Copperfield', 'Penn Jillette', 'Teller'];
+let magicians = ["David Copperfield", "Penn Jillette", "Teller"];
+function make_great(magicians) {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = "The Great " + magicians[i];
+    }
+}
 function show_magicians(magicians) {
     for (let i = 0; i < magicians.length; i++) {
         console.log(magicians[i]);
     }
 }
+make_great(magicians);
 show_magicians(magicians);
--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
-let magicians: string[] = ['David Copperfield', 'Penn Jillette', 'Teller'];
+let magicians: string[] = ["David Copperfield", "Penn Jillette", "Teller"];
+
+function make_great(magicians: string[]) {
+for (let i = 0; i < magicians.length; i++) {
+    magicians[i] = "The Great " + magicians[i];
+}
+}
 
 function show_magicians(magicians: string[]) {
     for (let i = 0; i < magicians.length; i++) {
         console.log(magicians[i]);
     }
 }
+
+make_great(magicians);
 
 show_magicians(magicians);
