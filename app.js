7fa1ac7319aed97e8a800ef2fca54eada0215b45
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:00:37 +0500

    Question number 22
    This file contains a solution to question number 22 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-var Item1 = {
-    name: "Mount Everest",
-    category: "Mountain",
-    description: "The highest mountain in the world."
-};
-var Item2 = {
-    name: "Nile River",
-    category: "River",
-    description: "The largest river in the world."
-};
-var Item3 = {
-    name: "Great Wall of China",
-    category: "Landmark",
-    description: " An ancient wall in China."
-};
-console.log("Item 1:", Item1);
-console.log("Item 2:", Item2);
-console.log("Item 3:", Item3);
+var numbers = [1, 2, 3, 4, 5];
+//Index Error
+console.log(numbers[10]);
+console.log(numbers[3]);
--- app.ts
+++ app.ts
@@ -1,30 +1,6 @@
-//Structure
-type Item = { 
-    name: string;
-    category: string;
-    description: string;
-};
+let numbers: number[] = [1, 2, 3, 4, 5];
 
-let Item1: Item = {
-    name: "Mount Everest",
-    category: "Mountain",
-    description: "The highest mountain in the world."
-};
+//Index Error
+console.log(numbers[10]);
 
-let Item2: Item = {
-    name: "Nile River",
-    category: "River",
-    description: "The largest river in the world."
-};
-
-let Item3: Item = {
-    name: "Great Wall of China",
-    category: "Landmark",
-    description: " An ancient wall in China."
-};
-
-console.log("Item 1:", Item1);
-console.log("Item 2:", Item2);
-console.log("Item 3:", Item3);
-
-
+console.log(numbers[3]);
