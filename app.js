b4ff4b8f47071e0265a957dcf6a89286a321e44e
Mariyam-Asif <160735103+Mariyam-Asif@users.noreply.github.com>
2024-04-04 05:10:19 +0500

    Question number 40
    This file contains a solution to question number 40 of the 45 questions assignment for the Governor Sindh Initiative IT course.

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 "use strict";
-function city_country(city, country) {
-    return city + ", " + country;
+function make_album(artist, title) {
+    return {
+        artist,
+        title
+    };
 }
-console.log(city_country("Islamabad", "Pakistan"));
-console.log(city_country("Tokyo", "Japan"));
-console.log(city_country("New York", "USA"));
+let album1 = make_album("Talha Anjum", "Talha Anjum Mixtape");
+let album2 = make_album("August D", "August D Mixtape");
+let album3 = make_album("BTS", "Proof");
+console.log(album1);
+console.log(album2);
+console.log(album3);
+function make_album2(artist, title, tracks) {
+    return {
+        artist,
+        title,
+        tracks
+    };
+}
+let album4 = make_album2("Atif Aslam", "Jal Pari", 8);
+let album5 = make_album2("Vital Signs", "Vital Signs 1", 9);
+let album6 = make_album2("Shehzad Roy", "Qismat Apney Haath Mein", 12);
+console.log(album4);
+console.log(album5);
+console.log(album6);
--- app.ts
+++ app.ts
@@ -1,6 +1,30 @@
-function city_country(city: string, country: string) {
-    return city + ", " + country;
-} 
-console.log(city_country("Islamabad", "Pakistan"));
-console.log(city_country("Tokyo", "Japan"));
-console.log(city_country("New York", "USA"));
+function make_album(artist: string, title: string) {
+    return {
+        artist,
+        title
+    };
+}
+
+let album1 = make_album("Talha Anjum", "Talha Anjum Mixtape");
+let album2 = make_album("August D", "August D Mixtape");
+let album3 = make_album("BTS", "Proof");
+
+console.log(album1);
+console.log(album2);
+console.log(album3);
+
+function make_album2(artist:string, title:string, tracks: number) {
+    return {
+        artist,
+        title,
+        tracks
+    };
+}
+
+let album4 = make_album2("Atif Aslam", "Jal Pari", 8);
+let album5 = make_album2("Vital Signs", "Vital Signs 1", 9);
+let album6 = make_album2("Shehzad Roy", "Qismat Apney Haath Mein", 12);
+
+console.log(album4);
+console.log(album5);
+console.log(album6);
